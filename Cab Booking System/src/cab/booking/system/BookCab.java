abc7d92512edd5d6260755edb057f5dd2effec0d
Kunal Tyagi <kunaltyagi93@gmail.com>
2020-10-01 23:11:43 +0530

    created initial classes in CBS

---
+++ Cab Booking System/src/cab/booking/system/BookCab.java
@@ -0,0 +1,228 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package cab.booking.system;
+
+
+import java.awt.*;
+import java.awt.event.*;
+import java.sql.*;
+import javax.swing.*;
+import net.proteanit.sql.DbUtils;
+
+public class BookCab extends JFrame{  //Forth
+
+    JTable table;
+    JLabel ReservationDetails,Source,Destination;
+    JButton show, b1, b2;
+    Choice c1, c2;
+    
+    public static void main(String[] args){
+        new BookCab("");
+    }
+    
+    public BookCab(String username){
+        
+        setTitle("Book Cab");
+	getContentPane().setBackground(Color.WHITE);
+	setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        
+        ReservationDetails = new JLabel("BOOK INTRACITY CAB");
+	ReservationDetails.setForeground(Color.BLUE);
+	ReservationDetails.setFont(new Font("Tahoma", Font.PLAIN, 31));
+	ReservationDetails.setBounds(280, 27, 359, 31);
+	add(ReservationDetails);
+
+	
+	setLayout(null);
+		
+	Source = new JLabel("SOURCE");
+	Source.setFont(new Font("Tahoma", Font.PLAIN, 19));
+	Source.setBounds(60, 100, 70, 27);
+	add(Source);
+	
+        Destination = new JLabel("DESTINATION");
+	Destination.setFont(new Font("Tahoma", Font.PLAIN, 19));
+	Destination.setBounds(350, 100, 150, 27);
+	add(Destination);
+		
+	
+		
+	show = new JButton("SHOW DETAILS");
+        show.setBackground(Color.BLACK);
+        show.setForeground(Color.WHITE);
+	show.setBounds(680, 100, 130, 27);
+	add(show);
+	
+	c1 = new Choice();
+        c1.setBounds(150, 100, 150, 27);
+        add(c1);
+      	
+	c2 = new Choice();
+        c2.setBounds(500, 100, 150, 27);
+        add(c2);
+        
+        try{
+            Conn c = new Conn();
+            ResultSet rs = c.s.executeQuery("select * from intracity");
+            while(rs.next()){
+                c1.add(rs.getString("source"));
+                c2.add(rs.getString("destination"));
+            }
+
+            rs.close();
+        }catch(SQLException e){}
+        
+        JLabel l9 = new JLabel("Name : ");
+	l9.setFont(new Font("Tahoma", Font.PLAIN, 19));
+	l9.setBounds(50, 200, 250, 27);
+	add(l9);
+        
+        JLabel l10 = new JLabel("");
+	l10.setFont(new Font("Tahoma", Font.PLAIN, 19));
+	l10.setBounds(200, 200, 150, 27);
+	add(l10);
+        
+        JLabel l11 = new JLabel("Username : ");
+	l11.setFont(new Font("Tahoma", Font.PLAIN, 19));
+	l11.setBounds(50, 250, 150, 27);
+	add(l11);
+        
+        JLabel l12 = new JLabel("");
+	l12.setFont(new Font("Tahoma", Font.PLAIN, 19));
+	l12.setBounds(200, 250, 350, 27);
+	add(l12);
+	
+        
+        JLabel l1 = new JLabel("Driver Name : ");
+	l1.setFont(new Font("Tahoma", Font.PLAIN, 19));
+	l1.setBounds(50, 300, 150, 27);
+	add(l1);
+        
+        JLabel l2 = new JLabel("");
+	l2.setFont(new Font("Tahoma", Font.PLAIN, 19));
+	l2.setBounds(200, 300, 150, 27);
+	add(l2);
+        
+        JLabel l3 = new JLabel("Price : ");
+	l3.setFont(new Font("Tahoma", Font.PLAIN, 19));
+	l3.setBounds(50, 450, 150, 27);
+	add(l3);
+        
+        JLabel l4 = new JLabel("");
+	l4.setFont(new Font("Tahoma", Font.PLAIN, 19));
+	l4.setBounds(200, 450, 150, 27);
+	add(l4);
+        
+        JLabel l5 = new JLabel("Car : ");
+	l5.setFont(new Font("Tahoma", Font.PLAIN, 19));
+	l5.setBounds(50, 350, 150, 27);
+	add(l5);
+        
+        JLabel l6 = new JLabel("");
+	l6.setFont(new Font("Tahoma", Font.PLAIN, 19));
+	l6.setBounds(200, 350, 150, 27);
+	add(l6);
+        
+        JLabel l7 = new JLabel("Route : ");
+	l7.setFont(new Font("Tahoma", Font.PLAIN, 19));
+	l7.setBounds(50, 400, 150, 27);
+	add(l7);
+        
+        JLabel l8 = new JLabel("");
+	l8.setFont(new Font("Tahoma", Font.PLAIN, 19));
+	l8.setBounds(200, 400, 350, 27);
+	add(l8);
+        
+        ImageIcon i1 = new ImageIcon(ClassLoader.getSystemResource("cab/booking/System/icons/intracab.png"));
+        Image i2 = i1.getImage().getScaledInstance(300, 300,Image.SCALE_DEFAULT);
+        ImageIcon i3 = new ImageIcon(i2);
+        
+        JLabel l16 = new JLabel(i3);
+        l16.setBounds(480, 150, 300, 300);
+        add(l16);
+			
+		
+	show.addActionListener(new ActionListener(){
+            public void actionPerformed(ActionEvent ae){
+		try{
+                    String src  = c1.getSelectedItem();
+                    String dst  = c2.getSelectedItem();
+                    
+                    Conn c = new Conn();
+                    
+                    String str = "select * from customer where username = '"+username+"'";
+                    String str2 = "select * from intracity where destination = '"+dst+"'";
+                    ResultSet rs = c.s.executeQuery(str2);
+                    
+                    if(rs.next()){
+                        l2.setText(rs.getString("driver"));
+                        l4.setText("Rs " + rs.getString("price"));
+                        l6.setText(rs.getString("car"));
+                    }
+                    
+                    rs = c.s.executeQuery(str);
+                    if(rs.next()){
+                        l10.setText(rs.getString("name"));
+                    }
+                    l8.setText(src + " --> " + dst);
+                    l12.setText(username);
+                    
+					
+		}catch(Exception e){
+                    e.printStackTrace();
+                }
+            }
+	});
+        
+        b1 = new JButton("Book CAB");
+        b1.setBackground(Color.BLACK);
+        b1.setForeground(Color.WHITE);
+        b1.setBounds(50, 500, 150, 30);
+        add(b1);
+        
+        b1.addActionListener(new ActionListener(){
+            public void actionPerformed(ActionEvent ae){
+		try{
+                    String src  = c1.getSelectedItem();
+                    String dst  = c2.getSelectedItem();
+                    String name = l2.getText();
+                    String price = l4.getText();
+                    String car = l6.getText();
+                    
+                    Conn c = new Conn();
+                    
+                    String str = "insert into intraCab values('"+username+"','"+name+"','"+src+"','"+dst+"','"+car+"','"+price+"')";
+                    c.s.executeUpdate(str);
+                    
+                    JOptionPane.showMessageDialog(null, "Cab Booked Successfully");
+                    setVisible(false);
+                    
+		}catch(Exception e){
+                    e.printStackTrace();
+                }
+            }
+	});
+        
+        
+        b2 = new JButton("Cancel");
+        b2.setBackground(Color.BLACK);
+        b2.setForeground(Color.WHITE);
+        b2.setBounds(250, 500, 150, 30);
+        add(b2);
+		
+        b2.addActionListener(new ActionListener(){
+            public void actionPerformed(ActionEvent ae){
+		setVisible(false);
+            }
+	});
+        
+        
+	setSize(860,600);
+        setLocation(600,220);
+	setVisible(true);
+		
+    }
+}
---
+++ Cab Booking System/src/cab/booking/system/ViewCabs.java
@@ -0,0 +1,186 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package cab.booking.system;
+
+
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.*;
+import java.io.File;
+
+public class ViewCabs extends JFrame implements Runnable {
+
+    JLabel l1, l2, l3, l4, l5, l6, l7, l8, l9, l10, l11, l12, l13, l14, l15, l16, l17, l18, l19;
+    JLabel caption;
+    Thread th;
+
+    public void run() {
+        try {
+
+            l2.setVisible(true);
+            caption.setText("Maruti Suzuki Ritz");
+            l2.add(caption);
+            Thread.sleep(2800);
+            l2.setVisible(false);
+            l3.setVisible(true);
+            caption.setText("Toyota Etios");
+            l3.add(caption);
+            l3.setVisible(true);
+            Thread.sleep(2800);
+            l3.setVisible(false);
+            l4.setVisible(true);
+            caption.setText("Toyota Fortuner");
+            l4.add(caption);
+            Thread.sleep(2800);
+            l4.setVisible(false);
+            l5.setVisible(true);
+            caption.setText("Maruti Suzuki Swift");
+            l5.add(caption);
+            Thread.sleep(2800);
+            l5.setVisible(false);
+            l6.setVisible(true);
+            caption.setText("Hyundai Santro");
+            l6.add(caption);
+            Thread.sleep(2800);
+            l6.setVisible(false);
+            l7.setVisible(true);
+            caption.setText("Auto");
+            l7.add(caption);
+            Thread.sleep(2800);
+            l7.setVisible(false);
+            l8.setVisible(true);
+            caption.setText("BMW X1");
+            l8.add(caption);
+            Thread.sleep(2800);
+            l8.setVisible(false);
+            l9.setVisible(true);
+            caption.setText("E-Rickshaw");
+            l9.add(caption);
+            Thread.sleep(2800);
+            l9.setVisible(false);
+            l10.setVisible(true);
+            caption.setText("Bike");
+            l10.add(caption);
+            Thread.sleep(2800);
+            l10.setVisible(false);
+            l11.setVisible(true);
+            caption.setText("Nissan Micra");
+            l11.add(caption);
+            Thread.sleep(2800);
+            l11.setVisible(false);
+            
+            this.setVisible(false);
+        } catch (Exception e) {
+            System.out.println(e);
+        }
+
+    }
+
+    public ViewCabs() {
+
+        setBounds(580, 220, 900, 700);
+        getContentPane().setBackground(new Color(220, 250, 250));
+        
+        th = new Thread(this);
+    
+        caption = new JLabel();
+        caption.setBounds(50, 550, 1000, 70);
+        caption.setForeground(Color.magenta);
+        caption.setFont(new Font("Tahoma", Font.PLAIN, 40));
+        add(caption);
+
+        
+        ImageIcon i1 = new ImageIcon(ClassLoader.getSystemResource("cab/booking/system/icons/cab1.png"));
+        Image i2 = i1.getImage().getScaledInstance(650, 434, Image.SCALE_DEFAULT);
+        ImageIcon i3 = new ImageIcon(i2);
+        l2 = new JLabel(i3);
+        l2.setBounds(0, 0, 900, 700);
+        add(l2);
+
+        setLayout(null);
+
+        ImageIcon i4 = new ImageIcon(ClassLoader.getSystemResource("cab/booking/system/icons/cab2.png"));
+        Image i5 = i4.getImage().getScaledInstance(650, 434, Image.SCALE_DEFAULT);
+        ImageIcon i6 = new ImageIcon(i5);
+        l3 = new JLabel(i4);
+        l3.setBounds(0, 0, 900, 700);
+        add(l3);
+        l3.setVisible(false);
+
+        ImageIcon i7 = new ImageIcon(ClassLoader.getSystemResource("cab/booking/system/icons/cab3.png"));
+        Image i8 = i7.getImage().getScaledInstance(650, 434, Image.SCALE_DEFAULT);
+        ImageIcon i9 = new ImageIcon(i8);
+        l4 = new JLabel(i9);
+        l4.setBounds(0, 0, 900, 700);
+        add(l4);
+        l4.setVisible(false);
+
+        ImageIcon i10 = new ImageIcon(ClassLoader.getSystemResource("cab/booking/system/icons/cab4.png"));
+        Image i11 = i10.getImage().getScaledInstance(650, 434, Image.SCALE_DEFAULT);
+        ImageIcon i12 = new ImageIcon(i11);
+        l5 = new JLabel(i12);
+        l5.setBounds(0, 0, 900, 700);
+        add(l5);
+        l5.setVisible(false);
+
+        ImageIcon i13 = new ImageIcon(ClassLoader.getSystemResource("cab/booking/system/icons/cab5.png"));
+        Image i14 = i13.getImage().getScaledInstance(650, 434, Image.SCALE_DEFAULT);
+        ImageIcon i15 = new ImageIcon(i14);
+        l6 = new JLabel(i15);
+        l6.setBounds(0, 0, 900, 700);
+        add(l6);
+        l6.setVisible(false);
+
+        ImageIcon i16 = new ImageIcon(ClassLoader.getSystemResource("cab/booking/system/icons/cab6.png"));
+        Image i17 = i16.getImage().getScaledInstance(650, 434, Image.SCALE_DEFAULT);
+        ImageIcon i18 = new ImageIcon(i17);
+        l7 = new JLabel(i18);
+        l7.setBounds(0, 0, 900, 700);
+        add(l7);
+        l7.setVisible(false);
+
+        ImageIcon i19 = new ImageIcon(ClassLoader.getSystemResource("cab/booking/system/icons/cab7.png"));
+        Image i20 = i19.getImage().getScaledInstance(650, 434, Image.SCALE_DEFAULT);
+        ImageIcon i21 = new ImageIcon(i20);
+        l8 = new JLabel(i21);
+        l8.setBounds(0, 0, 900, 700);
+        add(l8);
+        l8.setVisible(false);
+
+        ImageIcon i22 = new ImageIcon(ClassLoader.getSystemResource("cab/booking/system/icons/cab8.png"));
+        Image i23 = i22.getImage().getScaledInstance(650, 434, Image.SCALE_DEFAULT);
+        ImageIcon i24 = new ImageIcon(i23);
+        l9 = new JLabel(i24);
+        l9.setBounds(0, 0, 900, 700);
+        add(l9);
+        l9.setVisible(false);
+
+        ImageIcon i25 = new ImageIcon(ClassLoader.getSystemResource("cab/booking/system/icons/cab9.png"));
+        Image i26 = i25.getImage().getScaledInstance(650, 434, Image.SCALE_DEFAULT);
+        ImageIcon i27 = new ImageIcon(i26);
+        l10 = new JLabel(i27);
+        l10.setBounds(0, 0, 900, 700);
+        add(l10);
+        l10.setVisible(false);
+
+        ImageIcon i28 = new ImageIcon(ClassLoader.getSystemResource("cab/booking/system/icons/cab1.png"));
+        Image i29 = i28.getImage().getScaledInstance(650, 434, Image.SCALE_DEFAULT);
+        ImageIcon i30 = new ImageIcon(i29);
+        l11 = new JLabel(i30);
+        l11.setBounds(0, 0, 900, 700);
+        add(l11);
+        l11.setVisible(false);
+
+        th.start();
+
+    }
+
+    public static void main(String args[]) {
+        new ViewCabs().setVisible(true);
+
+    }
+
+}
